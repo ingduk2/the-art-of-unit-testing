ff7ac51cb7157b346f2500647ddf0d8c48f1e5da
ingduk2 <ingduk2@naver.com>
2025-03-26 08:40:51 +0900

    2.6 beforeEach() 함수 사용

--- ch2-first-test/__tests__/password-verifier1.spec.js
+++ ch2-first-test/__tests__/password-verifier1.spec.js
@@ -1,31 +1,61 @@
 const { PasswordVerifier1 } = require('../password-verifier1');
 
 describe('PasswordVerifier', () => {
+    let verifier;
+
+    beforeEach(() => {
+        verifier = new PasswordVerifier1();
+    });
+
     describe('with a failing rule', () => {
-        it ('has an error message based on the rule.reason', () => {
-            const verifier = new PasswordVerifier1();
-            const fakeRule = () => ({
-                passed: false,
-                reason: 'fake reason',
-            })
-
+        let fakeRule, errors;
+        
+        beforeEach(() => {
+            fakeRule = () => ({ passed: false, reason: 'fake reason' });
             verifier.addRule(fakeRule);
-            const errors = verifier.verify('any value');
+            errors = verifier.verify('any value');
+        });
 
+        it('has an error message based on the rule.reason', () => {
             expect(errors[0]).toContain('fake reason');
         });
 
-        it ('has exactly one error', () => {
-            const verifier = new PasswordVerifier1();
-            const fakeRule = () => ({
-                passed: false,
-                reason: 'fake reason',
-            })
-
-            verifier.addRule(fakeRule);
-            const errors = verifier.verify('any value');
-
+        it('has exactly one error', () => {
             expect(errors.length).toBe(1);
         });
     });
-})
+
+    describe('with a passing rule', () => {
+        let fakeRule, errors;
+
+        beforeEach(() => {
+            fakeRule = () => ({ passed: true, reason: '' });
+            verifier.addRule(fakeRule);
+            errors = verifier.verify('any rule');
+        });
+
+        it('has no errors', () => {
+            expect(errors.length).toBe(0);
+        });
+    });
+
+    describe('with a failing and a passing rule', () => {
+        let fakeRulePass, fakeRuleFail, errors;
+
+        beforeEach(() => {
+            fakeRulePass = () => ({ passed: true, reason: 'fake success' });
+            fakeRuleFail = () => ({ passed: false, reason: 'fake reason' });
+            verifier.addRule(fakeRulePass);
+            verifier.addRule(fakeRuleFail);
+            errors = verifier.verify('any value');
+        });
+
+        it('has one error', () => {
+            expect(errors.length).toBe(1);
+        });
+
+        it('error text belongs to failed rule', () => {
+            expect(errors[0]).toContain('fake reason');
+        });
+    });
+});
